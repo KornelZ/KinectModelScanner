3c52cba3bd0d84f184fc972069c699e224af7f1d
Averdose <MikolajSlon@wp.pl>
2018-05-23 19:44:29 +0200

    Mapper class further bugfixing

--- KinectModelScanner/ModelMapper.cs
+++ KinectModelScanner/ModelMapper.cs
@@ -13,6 +13,7 @@ namespace KinectModelScanner
         Kinect _kinect;
         Model _model;
         Matrix _view, _projection;
+        const int KINECT_DISTANCE = -600;
 
         public ModelMapper(Kinect kinect, ref Model model)
         {
@@ -24,20 +25,25 @@ namespace KinectModelScanner
         public void Map()
         {
             var depthStream = _kinect.KinectSensor.DepthStream;
-            _view = Matrix.CreateLookAt(new Vector3(-10, 0, 0), Vector3.Zero, Vector3.Up);
+            _view = Matrix.CreateLookAt(new Vector3(KINECT_DISTANCE, 0, 0), Vector3.Zero, Vector3.Up);
             _projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(depthStream.NominalVerticalFieldOfView), depthStream.FrameWidth / (float)depthStream.FrameHeight, 1, depthStream.TooFarDepth);
             DepthImagePixel[] depth = _kinect.Depth;
             foreach (MarkableVertex v in _model.Vertices)
             {
+                Microsoft.Xna.Framework.Vector4 extended = new Microsoft.Xna.Framework.Vector4(v.Position, 0);
                 Matrix tempMatrix = _projection * _view * _model.World;
-                var ver = Vector3.Transform(v.Position, tempMatrix);
-                float currDistance = Math.Abs(Vector3.Transform(v.Position, _model.World).X + 10);
+                Microsoft.Xna.Framework.Vector4 ver = Microsoft.Xna.Framework.Vector4.Transform(extended, tempMatrix);
+                ver = Microsoft.Xna.Framework.Vector4.Multiply(ver, (1 / ver.W));
+                float currDistance = Math.Abs(Vector3.Transform(v.Position, _model.World).X - KINECT_DISTANCE);
                 try
                 {
-                    var newDistance = Math.Abs(depth[calculateIndex(ver.X, ver.Y)].Depth);
-                    if (currDistance < newDistance)
+                    var newDistance = depth[calculateIndex(ver.X, ver.Y)].Depth;
+                    if (newDistance > 0)
                     {
-                        v.Marked = true;
+                        if (currDistance < newDistance)
+                        {
+                            v.Marked = true;
+                        }
                     }
 
                 }
@@ -49,6 +55,8 @@ namespace KinectModelScanner
 
         private int calculateIndex(float x, float y)
         {
+            x = ((x + 1) / 2) * _kinect.KinectSensor.DepthStream.FrameWidth;
+            y = ((y + 1) / 2) * _kinect.KinectSensor.DepthStream.FrameHeight;
             return Convert.ToInt32(y) * _kinect.KinectSensor.DepthStream.FrameWidth + Convert.ToInt32(x);
         }
     }
